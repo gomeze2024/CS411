163cf6e1fdd12970b6a04fb27841507066a104cd
gomeze2024 <gomeze@bu.edu>
2022-11-08 10:26:01 -0500

    adding character base to front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import logo from './character.PNG';
 import { motion } from "framer-motion";
 import { Refresh } from "./components/charbutton";
+import MyImage from "./character.PNG"
 
 function App() {
   const weather = {
@@ -15,14 +17,31 @@ function App() {
     name: 'Calendar',
     qo: "Calendar"
   }
+  const charstyles = {
+    background: "00",
+    borderRadius: 40,
+    width: 250,
+    padding: "0px 0px",
+    margin: "auto",
+    color: "#00",
+    outline: "none",
+    border: "none",
+    cursor: "pointer"
+  };
   return (
     <div
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          backgroundColor: '#f2f9ff'
         }}
       >
+      <motion.button style={charstyles}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95}}>
+        <img src={MyImage} className="App-logo" alt="logo" />
+      </motion.button>
       <Refresh 
        type = {weather}>
       </Refresh>
