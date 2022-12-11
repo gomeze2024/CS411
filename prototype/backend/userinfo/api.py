5924cd1840622e61ddadd6444e301b6a727a7bd3
gomeze2024 <gomeze@bu.edu>
2022-12-11 00:42:41 -0500

    fixing small bugs

---
+++ prototype/backend/userinfo/api.py
@@ -0,0 +1,58 @@
+import requests
+
+def set_comment(condition):
+  if (condition == "Thunderstorm"):
+    comment = "If you have to go outside, make sure to stay away from tall objects."
+  elif (condition == "Drizzle"):
+    comment = "Pitter patter. Pitter patter."
+  elif (condition == "Snow"):
+    comment = "Woah it's snowing. I love fresh snow"
+  elif (condition == "Clouds"):
+    comment = "Just another cloudy day."
+  elif (condition == "Clear"):
+    comment = "The weather's clear. Make sure to gets lots done!"
+  else:
+    comment = "Woah. Stay safe out there"
+  return comment
+
+# Function to handle the request to the OpenWeatherMap API
+def get_weather(zip_code):
+    api_key = "537275397b9037ad50e8da9814692add"
+    # URL for the OpenWeatherMap API
+    url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
+    # Send the request and get the response
+    response = requests.get(url)
+    condition = response.json()['weather'][0]['main']
+    comment = set_comment(condition)
+
+    weather_data = {
+        "location": response.json()['name'],
+        "temperature": response.json()['main']['temp'],
+        "description": response.json()['weather'][0]['description'],
+        "high": response.json()['main']['temp_max'],
+        "low": response.json()['main']['temp_min'],
+        "comment": comment
+    }
+    # Return the temperature in Fahrenheit
+    return weather_data
+
+
+# Function to handle the request to the NewsData.io API
+def get_news():
+  language = 'en'
+  category = 'world'
+
+  api_key = 'pub_13171ec2935c971700ae44d5ab454b14e2bf3'
+  url = "https://newsdata.io/api/1/news?apikey={}&language={}&category={}".format(api_key, language, category)
+  response = requests.get(url)
+  results = response.json()['results']
+  news_data = {
+    "News 1": (response.json()['results'][0]['title'], response.json()['results'][0]['country'][0], response.json()['results'][0]['description']),
+    "News 2": (response.json()['results'][1]['title'], response.json()['results'][1]['country'][0], response.json()['results'][1]['description']),
+    "News 3": (response.json()['results'][2]['title'], response.json()['results'][2]['country'][0], response.json()['results'][2]['description']),
+    "News 4": (response.json()['results'][3]['title'], response.json()['results'][3]['country'][0], response.json()['results'][3]['description']),
+    "News 5": (response.json()['results'][4]['title'], response.json()['results'][4]['country'][0], response.json()['results'][4]['description'])
+    }
+  
+  return news_data
+
--- prototype/backend/userinfo/models.py
+++ prototype/backend/userinfo/models.py
@@ -8,8 +8,8 @@ class UserInfo(models.Model):
     id = models.CharField(max_length=320, default=0, primary_key=True)
     user_email = models.CharField(max_length=320)
     location = models.TextField(default= "We don't have your location yet. Give it to us please")
-    weather = models.JSONField(default= dict)
-    news = models.JSONField(default= dict)
+    weather = models.JSONField(default= {})
+    news = models.JSONField(default= {})
     color = models.TextField(default=  "We can't update your color without you updating your color. Give it to us please")
 
     def _str_(self):
@@ -18,5 +18,5 @@ class UserInfo(models.Model):
     def save(self, *args, **kwargs):
         zip_string = self.location
         self.weather = get_weather(zip_string)
-        self.news = get_news ()
+        self.news = get_news()
         super().save(*args, **kwargs) 
